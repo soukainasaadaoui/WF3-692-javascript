768fd8317713540403c5bbf91a93398e861b42ad
= <=>
2021-01-25 15:54:33 +0100

    add correction exercice2

--- js/exercice2.js
+++ js/exercice2.js
@@ -2,3 +2,8 @@ var color = ["red", "blue", "white", "black", "orange", "tomato", "yellow"];
 
 // @ToDo => parcourir le tableau de conleur et l'afficher dans la console
 // avec une boucle for
+
+// correction
+for(var i = 0; i < color.length; i = i + 1){
+    console.log(color[i]);
+}
