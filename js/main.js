afe8213fa7be1596c3c74ad0824b8448aa31e3cf
myhed <benrhouma.myhed@gmail.com>
2021-01-25 11:31:03 +0100

    init project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    // Avant
+    var p = document.getElementsByTagName('p');
+    var div = document.getElementsByTagName('div');
+    var greenP = document.getElementById('green');
+    var p1 = document.getElementsByClassName('p1');
+    console.log("greenP: ",greenP, "div: ", div, "all paragraph: ", p, "p1: ", p1);
+    // Aujourd'hui
+    var pWithQuerySelectorByTagName = document.querySelector('p');
+    var pWithQuerySelectorById = document.querySelector('#green');
+    var pWithQuerySelectorByClassName =  document.querySelector('.p1');
+    console.log("pWithQuerySelector: ", pWithQuerySelectorByTagName); 
+    console.log("pWithQuerySelectorById: ", pWithQuerySelectorById);
+    console.log("pWithQuerySelectorByClassName: ", pWithQuerySelectorByClassName);
+    
+    // Pour pouvoir récupérer plusieurs éléments avec querySelector
+    // Il faut utiliser querySelectorAll()
+    var getAllParagrapheWithQuerySelectorAll = document.querySelectorAll('p');
+    console.log("getAllParagrapheWithQuerySelectorAll: ", getAllParagrapheWithQuerySelectorAll)
+
+}
