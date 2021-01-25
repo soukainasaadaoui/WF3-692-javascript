06ffa6dd33dec24d21b282bc51813c40875e54d1
= <=>
2021-01-25 16:15:48 +0100

    add exercice 3 iterate on object(collection, dictionnary)

---
+++ js/exercice3.js
@@ -0,0 +1,18 @@
+var fruits = {
+    pomme: 4,
+    poire: 0,
+    banane: 10,
+    tomate: 6
+}
+
+// Itérer sur l'objet fruits
+/**
+ * On s'attendra alors à
+ * Output console: key: pomme value: 4
+ * Output console: key: poire: value: 0 
+ * Output console: key: banane value: 10
+ * Output console: key: pomme value: 6
+ */
+
+ // @ToDo 
+ 
